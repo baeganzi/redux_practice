e9eb9971482a53d0959812990f74f7c040486ec0
baeganzi <60914142+baeganzi@users.noreply.github.com>
2022-01-06 21:37:52 +0900

    clock추가

--- nextjs-app/src/app/store.ts
+++ nextjs-app/src/app/store.ts
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-
-export const store = configureStore({
-	reducer: {
-		counter: counterReducer,
-	},
+import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import counterReducer from "@/features/counter/counterSlice";
+import clockReducer from "@/features/clock/clockSlice";
+import { createWrapper } from "next-redux-wrapper";
+const rootReducer = combineReducers({
+	counter: counterReducer,
+	clock: clockReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export const makeStore = () => {
+	return configureStore({
+		reducer: rootReducer,
+		devTools: process.env.NODE_ENV !== "production",
+	});
+};
+export const store = makeStore();
+// export const store = configureStore({
+// 	reducer: {
+// 		counter: counterReducer,
+// 	},
+// });
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `AppState` and `AppDispatch` types from the store itself
+export type AppState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action<string>>;
+
+export const wrapper = createWrapper<AppStore>(makeStore, { debug: false });
